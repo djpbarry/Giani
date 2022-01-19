16b4f16a2c3658bf9fed61b0aa72fca4a3947bfd
Dave Barry <david.barry@crick.ac.uk>
2022-01-19 15:13:27 +0000

    Added some more Javadoc comments

--- src/main/java/net/calm/giani/exec/PipelineBuilder.java
+++ src/main/java/net/calm/giani/exec/PipelineBuilder.java
@@ -29,8 +29,21 @@ import net.calm.iaclasslibrary.Process.Segmentation.MultiThreadedWatershed;
 
 import java.util.Properties;
 
+/**
+ * Main entry point for building a GIANI pipeline. To build your own custom pipeline, extend this class and override
+ * the buildFullPipeline method
+ *
+ * @author Dave Barry
+ * @since 1.0.0
+ */
 public class PipelineBuilder {
 
+    /**
+     * Returns the default blob detector process used by GIANI
+     *
+     * @param props the properties containing key-value parameter-setting pairs to be associated with the process
+     * @return a new {@link MultiThreadedMaximaFinder}
+     */
     public static MultiThreadedMaximaFinder getDefaultMaximaFinder(Properties props) {
         String[] propLabels = new String[MultiThreadedMaximaFinder.N_PROP_LABELS];
         propLabels[MultiThreadedMaximaFinder.CHANNEL_SELECT] = GianiDefaultParams.BLOB_NUC_CHAN_SELECT_LABEL;
@@ -50,6 +63,13 @@ public class PipelineBuilder {
         return process;
     }
 
+    /**
+     * Returns the default top hat filter process used by GIANI
+     *
+     * @param props the properties containing key-value parameter-setting pairs to be associated with the process
+     * @param inputs the processes whose outputs will be used as inputs for this process
+     * @return a new {@link MultiThreadedTopHatFilter}
+     */
     public static MultiThreadedTopHatFilter getDefaultNucTopHatFilteringProcess(Properties props, MultiThreadedProcess[] inputs) {
         String[] propLabels = new String[MultiThreadedTopHatFilter.N_PROP_LABELS];
         propLabels[MultiThreadedTopHatFilter.CHANNEL_LABEL] = GianiDefaultParams.NUC_SEG_CHAN_SELECT_LABEL;
@@ -61,6 +81,12 @@ public class PipelineBuilder {
         return process;
     }
 
+    /**
+     * Returns the default nuclear Gaussian filter process used by GIANI
+     *
+     * @param props the properties containing key-value parameter-setting pairs to be associated with the process
+     * @return a new {@link MultiThreadedGaussianFilter}
+     */
     public static MultiThreadedGaussianFilter getDefaultNucFilteringProcess(Properties props) {
         String[] propLabels = new String[MultiThreadedGaussianFilter.N_PROP_LABELS];
         propLabels[MultiThreadedGaussianFilter.CHANNEL_LABEL] = GianiDefaultParams.NUC_SEG_CHAN_SELECT_LABEL;
@@ -71,6 +97,14 @@ public class PipelineBuilder {
         return process;
     }
 
+    /**
+     * Returns the default nuclear segmentation process used by GIANI
+     *
+     * @param props the properties containing key-value parameter-setting pairs to be associated with the process
+     * @param inputs the processes whose outputs will be used as inputs for this process
+     * @param cells instance of an {@link Objects3DPopulation} that will store the resulting segmentation
+     * @return a new {@link MultiThreadedWatershed}
+     */
     public static MultiThreadedWatershed getDefaultNucSegmenter(Properties props, MultiThreadedProcess[] inputs, Objects3DPopulation cells) {
         String[] propLabels = new String[MultiThreadedWatershed.N_PROP_LABELS];
         propLabels[MultiThreadedWatershed.SERIES_LABEL] = GianiDefaultParams.SERIES_SELECT_LABEL;
@@ -89,6 +123,12 @@ public class PipelineBuilder {
         return process;
     }
 
+    /**
+     * Returns the default cell Gaussian filter process used by GIANI
+     *
+     * @param props the properties containing key-value parameter-setting pairs to be associated with the process
+     * @return a new {@link MultiThreadedGaussianFilter}
+     */
     public static MultiThreadedGaussianFilter getDefaultCellFilteringProcess(Properties props) {
         MultiThreadedGaussianFilter process = new MultiThreadedGaussianFilter(null);
         String[] propLabels = new String[MultiThreadedGaussianFilter.N_PROP_LABELS];
@@ -99,6 +139,14 @@ public class PipelineBuilder {
         return process;
     }
 
+    /**
+     * Returns the default cell segmentation process used by GIANI
+     *
+     * @param props the properties containing key-value parameter-setting pairs to be associated with the process
+     * @param inputs the processes whose outputs will be used as inputs for this process
+     * @param cells instance of an {@link Objects3DPopulation} that will store the resulting segmentation
+     * @return a new {@link MultiThreadedWatershed}
+     */
     public static MultiThreadedWatershed getDefaultCellSegmenter(Properties props, MultiThreadedProcess[] inputs, Objects3DPopulation cells) {
         String[] propLabels = new String[MultiThreadedWatershed.N_PROP_LABELS];
         propLabels[MultiThreadedWatershed.SERIES_LABEL] = GianiDefaultParams.SERIES_SELECT_LABEL;
@@ -119,6 +167,14 @@ public class PipelineBuilder {
         return process;
     }
 
+    /**
+     * Returns the default object measurement process used by GIANI
+     *
+     * @param props the properties containing key-value parameter-setting pairs to be associated with the process
+     * @param inputs the processes whose outputs will be used as inputs for this process
+     * @param cells instance of an {@link Objects3DPopulation} that will store the resulting segmentation
+     * @return a new {@link MultiThreadedWatershed}
+     */
     public static MultiThreadedROIConstructor getDefaultMeasure(Properties props, MultiThreadedProcess[] inputs, Objects3DPopulation cells) {
         String[] propLabels = new String[MultiThreadedROIConstructor.N_PROP_LABELS];
         propLabels[MultiThreadedROIConstructor.SERIES_LABEL] = GianiDefaultParams.SERIES_SELECT_LABEL;
@@ -133,6 +189,13 @@ public class PipelineBuilder {
         return process;
     }
 
+    /**
+     * Assemble a complete GIANI pipeline
+     *
+     * @param props the parameter-value pairs to be used for all processes in the pipeline
+     * @param cells instance of an {@link Objects3DPopulation} that will store the resulting segmentations
+     * @return a new {@link ProcessPipeline}
+     */
     public ProcessPipeline buildFullPipeline(Properties props, Objects3DPopulation cells) {
         ProcessPipeline pipeline = new ProcessPipeline();
 
